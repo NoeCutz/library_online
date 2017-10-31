c059702a9ccebe021b5690fc320ffb777a26964d
Noe.Cutz <practica2.sistemas@galletasdonde.com>
2017-10-31 09:20:28 -0600

    Resuelto el problema de los eventos relacionados con el login del usuario en todas las páginas

--- js/eventos.js
+++ js/eventos.js
@@ -27,7 +27,6 @@ window.onload = function () {
         perfil.onclick = function () {
             if (sessionStorage.username) {
                 logout();
-                location.href = "index.html"
             } else {
                 location.href = "login.html"
             }
@@ -92,6 +91,7 @@ function logout() {
     if(answer){
         sessionStorage.removeItem("username");
         alert("Tu sesion se ha cerrado");
+        location.href = "login.html"
     }
 
 
@@ -168,6 +168,43 @@ function existsTelephone(telephone) {
     return false;
 }
 
+//Shopping list
 
 
+function loadCart() {
+    var keys= Object.keys(localStorage);
+    for (var i=0; i<keys.length; i++){
+        var element= JSON.parse(localStorage.getItem(keys[i]));
+        addElementsToBody(element);
+    }
+    showTotal(keys.length);
+}
+
+function addElementsToBody(element) {
+    var inserElementCode = "<tr><td>" +element[0]+"</td>";
+    inserElementCode+= "<td>"+ element[1]+"</td>";
+    inserElementCode+= "<td><input type='button' class='delete' value='Eliminar' onclick=\"deleteElement(this)\"></td>";
+    document.getElementById("tableBody").innerHTML +=inserElementCode;
+}
+
+function deleteElement(button) {
+    var row = button.parentNode.parentNode;
+    localStorage.removeItem(row.cells[0].innerHTML);
+    location.reload();
+}
+function showTotal(length) {
+    var table = document.getElementById("shopping_cart");
+    var total=0;
+    if (length>0){
+        for(var i=0;i<length; i++){
+            total+= parseFloat(table.rows[i+1].cells[1].innerHTML);
+        }
+    }
+    document.getElementById("cost_total").innerHTML=total.toString();
+}
+
+function emptyCart() {
+    localStorage.clear();
+    location.reload();
+}
 
