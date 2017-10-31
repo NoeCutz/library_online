b4ef0548b5466c479e23a7ed596cd54b08897623
Mariana Rocha <mnrt.cr@gmail.com>
2017-10-31 07:56:52 -0600

    Carrito completo, estilos agregados

--- js/shop_list.js
+++ js/shop_list.js
@@ -1,32 +1,36 @@
-
-function loadBooks(){ //sets books for test
-    var book0 = ["Alquimista","350"]; //Datos de libro a guardar
-    var book1=["Principito","123"];
-    var book2=["Soy un gato","600"];
-    localStorage.setItem("Alquimista", JSON.stringify(book0)); //metodo para guardar, key debe ser unico
-    localStorage.setItem("Principito", JSON.stringify(book1));
-    localStorage.setItem("Soy un gato", JSON.stringify(book2));
-}
-
-
-window.onload = function() {
-    loadBooks();
+function loadCart() {
     var keys= Object.keys(localStorage);
     for (var i=0; i<keys.length; i++){
         var element= JSON.parse(localStorage.getItem(keys[i]));
         addElementsToBody(element);
     }
-};
+    showTotal(keys.length);
+}
 
 function addElementsToBody(element) {
     var inserElementCode = "<tr><td>" +element[0]+"</td>";
     inserElementCode+= "<td>"+ element[1]+"</td>";
-    inserElementCode+= "<td><input type='button' class='delete' value='Eliminar' onclick=\"deleteElement("+element[0] +")\"></td>";
+    inserElementCode+= "<td><input type='button' class='delete' value='Eliminar' onclick=\"deleteElement(this)\"></td>";
     document.getElementById("tableBody").innerHTML +=inserElementCode;
 }
 
-function deleteElement(key) {
-    console.log(key.toString());
-    localStorage.removeItem(key);
+function deleteElement(button) {
+    var row = button.parentNode.parentNode;
+    localStorage.removeItem(row.cells[0].innerHTML);
+    location.reload();
+}
+function showTotal(length) {
+    var table = document.getElementById("shopping_cart");
+    var total=0;
+    if (length>0){
+        for(var i=0;i<length; i++){
+            total+= parseFloat(table.rows[i+1].cells[1].innerHTML);
+        }
+    }
+    document.getElementById("cost_total").innerHTML=total.toString();
+}
 
+function emptyCart() {
+    localStorage.clear();
+    location.reload();
 }
